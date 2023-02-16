d3e55a1cabe4e9f84bb3baeec1db39f13ac27038
Aurel6021 <74885177+Aurel6021@users.noreply.github.com>
2023-02-16 23:53:06 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0d674289d357c61a4fd685ee14b75a8c",
-"/": "0d674289d357c61a4fd685ee14b75a8c",
+"index.html": "a875dda0c9d63866fb61828e1b63eead",
+"/": "a875dda0c9d63866fb61828e1b63eead",
 "main.dart.js": "c25a1743c80758c9191a12e018d7e2eb",
 "manifest.json": "a085d4e65fff5e24646ef87566b2ce5f",
 "version.json": "c654dd307ee3bb296f61f703ab76fa29"
